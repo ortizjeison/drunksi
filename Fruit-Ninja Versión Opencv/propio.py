e29eb3b5b8d9c9683ab58c7e5fa40ff4f426be92
JarodMejia <32924220+JarodMejia@users.noreply.github.com>
2017-10-25 11:41:44 -0500

    Fruit ninja juego
    para basarnos con este codigo en la intro ademas para la recepcion e interpretacion de las imagenes

---
+++ Fruit-Ninja Versión Opencv/propio.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from pygame import *
+from math import *
+from random import randint, choice, uniform
+from time import localtime as local
+from time import time as clock
+import datetime
+import cv2
+import numpy as np
+import pygame
+
+def main():
+	
